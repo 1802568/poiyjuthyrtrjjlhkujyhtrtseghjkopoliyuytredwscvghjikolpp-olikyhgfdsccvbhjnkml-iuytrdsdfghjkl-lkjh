a2ba62574401f86b0a67b16fdb5d90b9e7a30bdf
1802568 <76923976+1802568@users.noreply.github.com>
2021-04-28 19:11:25 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# poiyjuthyrtrjjlhkujyhtrtseghjkopoliyuytredwscvghjikolpp-olikyhgfdsccvbhjnkml-iuytrdsdfghjkl-lkjh
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var engine, world;
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  hi1 = new box(330,235,30,40)
+  hi2 = new box(360,235, 30,40)
+  hi3 = new box(390,235,30,40)
+  hi4 = new box(420,235, 30,40)
+  hi5 = new box(450,235, 30,40)
+}
+
+function draw() {
+  Engine.update(engine);
+  background(0);  
+  drawSprites();
+  
+  hi1.display()
+  hi2.display()
+  hi3.display()
+  hi4.display()
+  hi5.display()
+}
