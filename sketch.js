70c6fb27a0e0bdb2d91fb0ab2c6e3afe04a394f2
1802568 <76923976+1802568@users.noreply.github.com>
2021-05-02 14:16:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
-const Engine = Matter.Engine;
+
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint
 var engine, world;
+var boing
 function setup() {
-  engine = Engine.create();
+  
   world = engine.world;
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
+  boing=createSprite(20,20,20,20)
   hi1 = new box(330,235,30,40)
   hi2 = new box(360,235, 30,40)
   hi3 = new box(390,235,30,40)
   hi4 = new box(420,235, 30,40)
   hi5 = new box(450,235, 30,40)
+  hex=new box(20,20,20,20)
+  Slingshot1= new Slingshot(hex.body,ground.body);
 }
 
 function draw() {
@@ -24,4 +29,9 @@ function draw() {
   hi3.display()
   hi4.display()
   hi5.display()
+  hex.display()
+}
+function keyPressed(){
+  if(keyCode===32)
+  me.attach(hex.body);
 }
